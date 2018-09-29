aec127d76c043856dc711e7594adba7d58164136
Vu Duong <vuduong191@gmail.com>
2018-09-29 11:29:48 -0700

    Generating article information

--- main.js
+++ main.js
@@ -28,7 +28,29 @@ var articleURL = [
 var articleDate = ["2018-08-17","2018-08-17","2018-08-17"]
 // My code will go between these lines.
 // ------------------------------
+    var articleRow = $("<div>")
+    articleRow.addClass("article-row")
+    var artTitle = $("<p>")
+    artTitle.addClass("title")
+    var artDate = $("<p>")
+    artDate.addClass("article-date")
+    var artSum = $("<p>")
+    artSum.addClass("summary")    
+    var artIMG = $("<img>")
+    artIMG.addClass("thumbnail")
+    var moreLink = $("<a>")
+    moreLink.attr("target","_blank")
+    moreLink.text("More...")
+for(var i=0;i<thumbnail.length;i++){
+    artTitle.text(title[i])
+    artDate.text(articleDate[i])
+    artSum.text(summary[i])
+    artIMG.attr("src",thumbnail[i])
+    moreLink.attr("href",articleURL[i])
+    artSum.append(moreLink)
+    $(".article-row").append(artTitle, artDate, artIMG,artSum)
 
+}
 
 // ------------------------------
 
