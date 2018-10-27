76be035eae49f8037764c2fdbbdbbd3d2a816e0a
Vu Duong <vuduong191@gmail.com>
2018-10-27 01:43:48 -0700

    final touch

--- main.js
+++ main.js
@@ -1,56 +1,88 @@
 $(document).ready(function() {
-// Hi Omar, assuming you have these input below. Here I created variables with predefined values, just for you to test your codes. It's my job with JS to generate these for you
-var inputTerm = "tesla"
-var inputStartYear1 = "2010"
-var inputEndYear1 = "2018"
-// @Omar, your codes go between these two lines
-// ------------------------------
+var inputTerm;
+var inputStartYear1;
+var inputEndYear1;
+var noOfRecord;
 
+$("#search").on("click", function(){
+    inputTerm = $("#inputterm").val().trim();
+    inputStartYear1 = $("#inputstartyear").val().trim();
+    inputEndYear1 = $("#inputendyear").val().trim();
+    noOfRecord = parseFloat($("#limit").val().trim());
+console.log(inputEndYear1+" "+inputStartYear1+" "+inputTerm+" "+noOfRecord)
+//  NYTimes search API
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+    'api-key': "24778e6dc7a245e484e24d080235ddb0",
+    'q': inputTerm,
+    'begin_date': inputStartYear1+"0101",
+    'end_date': inputEndYear1+"1231",
+    'sort': "newest",
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+    var result = result.response.docs;
+    var title = [];
+    var summary = [];
+    var articleURL= [];
+    var articleDate = [];
+    for(var i = 0; i<noOfRecord;i++){
+        if (result[i]!==null) {
+            title.push(result[i].headline.main)
+            summary.push(result[i].snippet)
+            articleURL.push(result[i].web_url)
+            articleDate.push(result[i].pub_date.substring(0,10))
+        }
+    }
+    console.log(title.length)
 
-// ------------------------------
-// And this is what I need from you, aka your output. Here I created some arrays with predefined values, just for me to check my pieces.
-var thumbnail = [
-    "https://static01.nyt.com/images/2018/08/17/business/17MUSK1/17MU-thumbWide.jpg",
-    "https://static01.nyt.com/images/2018/08/17/business/17MUSK1/17MU-thumbWide.jpg",
-    "https://static01.nyt.com/images/2018/08/17/business/17MUSK1/17MU-thumbWide.jpg"
-]
-var title = ["Congress, Look at Tesla","Congress, Look at Tesla","Congress, Look at Tesla"]
-var summary = [
-    "Why can’t Republicans in Congress react like the Tesla board of directors to impulsive and irrational tweets?",
-    "Why can’t Republicans in Congress react like the Tesla board of directors to impulsive and irrational tweets?",
-    "Why can’t Republicans in Congress react like the Tesla board of directors to impulsive and irrational tweets?"        
-]
-var articleURL = [
-    "https://www.nytimes.com/2018/08/17/opinion/letters/tesla-musk-trump.html",
-    "https://www.nytimes.com/2018/08/17/opinion/letters/tesla-musk-trump.html",
-    "https://www.nytimes.com/2018/08/17/opinion/letters/tesla-musk-trump.html"
-]
-var articleDate = ["2018-08-17","2018-08-17","2018-08-17"]
-// My code will go between these lines.
-// ------------------------------
-    var articleRow = $("<div>")
-    articleRow.addClass("article-row")
-    var artTitle = $("<p>")
-    artTitle.addClass("title")
-    var artDate = $("<p>")
-    artDate.addClass("article-date")
-    var artSum = $("<p>")
-    artSum.addClass("summary")    
-    var artIMG = $("<img>")
-    artIMG.addClass("thumbnail")
-    var moreLink = $("<a>")
-    moreLink.attr("target","_blank")
-    moreLink.text("More...")
-for(var i=0;i<thumbnail.length;i++){
-    artTitle.text(title[i])
-    artDate.text(articleDate[i])
-    artSum.text(summary[i])
-    artIMG.attr("src",thumbnail[i])
-    moreLink.attr("href",articleURL[i])
-    artSum.append(moreLink)
-    $(".article-row").append(artTitle, artDate, artIMG,artSum)
+    //     var articleRow = $("<div>")
+    //     articleRow.addClass("article-row")
+    //     var artTitle = $("<p>")
+    //     artTitle.addClass("title")
+    //     var artDate = $("<p>")
+    //     artDate.addClass("article-date")
+    //     var artSum = $("<p>")
+    //     artSum.addClass("summary")    
+    //     var moreLink = $("<a>")
+    //     moreLink.attr("target","_blank")
+    //     moreLink.text(" More...")
 
-}
+    // for(var i=0;i<title.length;i++){
+    //     artTitle.text(title[i])
+    //     artDate.text(articleDate[i])
+    //     artSum.text(summary[i])
+    //     moreLink.attr("href",articleURL[i])
+    //     artSum.append(moreLink)
+    //     articleRow.empty()
+    //     articleRow.append(artTitle, artDate, artSum)
+    //     console.log("row "+i+ articleRow)
+    //     $("#article-container").append(articleRow);
+    //     console.log("this is "+i+JSON.stringify($("#article-container")))
+    // }
+
+    // The attempt above not works, and I don't know why
+    var articleRow = "";
+    for (var i=0;i<title.length;i++){
+        articleRow = "<div class=article-row>"+
+            "<p class = title>"+ title[i] +"</p>"+
+            "<p class = article-date>"+ articleDate[i] +"</p>"+
+            "<p class = summary>"+ summary[i] +
+            "<a target='_blank' href='"+articleURL[i]+"'> More...</a></p></div";
+        console.log(articleRow)
+        $("#article-container").append(articleRow);
+    }
+}).fail(function(err) {
+  throw err;
+});
+
+// End of ajax
+})
+$("#clear").on("click", function(){
+    $("#article-container").empty()
+})
 
 // ------------------------------
 
